6e5b4544d13c17df13b837867e4abcbb0d9c2864
A.A.A <adewunmiabdulhameed5@gmail.com>
2025-05-24 22:20:28 +0100

    added a classlist to the container and styled it

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -37,9 +37,14 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link " data-product-id = ${matchingProduct.id}>
               Update
             </span>
+            <input class = "quantity-input">
+            <span class = "save-quantity-link link-primary">
+            Save
+            </span>
+
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id = ${matchingProduct.id}>
               Delete
             </span>
@@ -110,6 +115,19 @@ document.querySelectorAll('.js-delete-link')
       container.remove();
       updateCartQuantity();
     });
+  });
+
+document.querySelectorAll('.js-update-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const {productId} = link.dataset;
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      container.classList.add('is-editing-quantity');
+
+      console.log(productId);
+    })
   });
 
 function  updateCartQuantity () {
